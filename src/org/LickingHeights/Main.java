bca68c2d66c146da7155a02713775789addba67a
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-10-15 09:40:28 -0400

    added "theName" method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,12 +3,14 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello World");
+        String answer = theName("Tim");
 
-    System.out.println("Hello World");
-
-
-
-
-
+        System.out.println(answer);
     }
-}
+
+    public static String theName(String name){
+        return "Hello " + name;
+
+        }
+    }
