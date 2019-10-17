b070ca25d7d83b322087258ca47b4dfacea763ed
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-10-17 09:26:17 -0400

    added "findMax" method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -6,28 +6,49 @@ public class Main {
         System.out.println("Hello World");
         String aName = theName("Tim");
         System.out.println(aName);
-        int a,b;
-        a=5;
-        b=6;
-        int answer = multiply(a,b);
+        int a, b;
+        a = 5;
+        b = 6;
+        int answer = multiply(a, b);
         System.out.println(answer);
-        int c,d;
-        c=5;
-        d=9;
-        double answer2 =divide(c,d);
+        int c, d;
+        c = 5;
+        d = 9;
+        double answer2 = divide(c, d);
         System.out.println(answer2);
+        int e, f;
+        e = 8;
+        f = 9;
+        int answer3 = findMax(e, f);
+        System.out.println(answer3);
+
+
     }
 
-    public static String theName(String name){
+    public static String theName(String name) {
         return "Hello " + name;
 
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(double c, double d) {
+        return c / d;
+    }
+
+    public static int findMax(int e, int f) {
+        if (f > e) {
+            return f;
+        } else if (e > f) {
+            return e;
         }
+          else {
+                return e;
+            }
 
-        public static int multiply(int a,int b) {
-            return a*b;
-    }
-    public static int divide(int c,int d) {
-        return c/d;
-    }
+
 
     }
+}
